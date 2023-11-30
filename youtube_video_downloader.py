ff012b83e313c067e204936db61d122d72923b72
MATEO CELA <112934949+mateo1mc@users.noreply.github.com>
2023-11-30 10:44:11 +0100

    Update youtube_video_downloader.py
    save to Downloads

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -3,6 +3,7 @@ from pytube.cli import on_progress
 import pytube.request
 import random
 import time
+import os
 
 pytube.request.default_range_size = 500000
 
@@ -13,7 +14,7 @@ def typewrite(num1, num2, text):
         print(c, end='', flush=True)
 
 
-def completed(a, b):
+def completed(stream):
     completed = '\nDownload Completed!\n'
     size = 'File Size: ' + str(stream.filesize / 1000000) + ' Mb\n'
     title = 'Title: ' + stream.title + '\n'
@@ -26,11 +27,16 @@ def completed(a, b):
         typewrite(.05, .1, t)
         print('-' * 60)
 
+
 url = input("\nPlease enter the URL: ")
 try:
-    yt = YouTube(url, on_progress_callback=on_progress, on_complete_callback=completed)
+    yt = YouTube(url, on_progress_callback=on_progress)
     stream = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
     typewrite(.05, 1, 'Download is starting...\n')
-    stream.download()
-except:
-    print('Something Went Wrong! Please Try Again!')
+
+    download_path = os.path.join(os.path.expanduser("~"), "Downloads")  # Path to the Downloads folder
+
+    stream.download(output_path=download_path)
+    completed(stream)
+except Exception as e:
+    print('Something Went Wrong! Please Try Again:', e)
